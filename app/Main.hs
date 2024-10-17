7fd6f2da516c4a0f263f13b0d1fd3da938cfeb31
Turtel216 <papadimitrios216@gmail.com>
2024-10-17 17:15:08 +0300

    Added main module

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
-module Main (main) where
+module Main where
 
-import Lib
+import  Control.Monad.IO.Class
+import  AsciiConverter.Lib ( Config (Config, imageColor, imageWidth), convertToAscii, resizeImage)
+import  Graphics.Image     ( readImage )
+
 
 main :: IO ()
-main = someFunc
+main = do
+    let config = Config{imageWidth = 100, imageColor = False}
+    image <- readImage "yourimage.jpg"
+
+    case image of
+        Left _ -> putStrLn "Couldn't read the image"
+        Right img -> do
+            let resizedImg = resizeImage (imageWidth config) img
+            converted <- liftIO $ convertToAscii resizedImg config
+            putStrLn converted
